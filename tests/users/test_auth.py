d21cdd5564e3778d9e07de0e39a945c168aced23
mau <mauraneglaude@gmail.com>
2025-06-03 07:27:40 +0200

    test(users): fix bad password for tests
    Signed-off-by: mau <mauraneglaude@gmail.com>

--- tests/users/test_auth.py
+++ tests/users/test_auth.py
@@ -1,43 +1,47 @@
 import datetime
 
 import httpx
-from django.test import TestCase
+from django.test import TestCase, Client
 
 from apps.users.models import User, DoctorProfile, PatientProfile
 
 
 class TestAuthentication(TestCase):
-    def setUp(self):
+    @classmethod
+    def setUp(cls):
         doctor_user = User.objects.create_user(
             username='doctor', email='doctor@email.com', password='doctor_password', role='doctor'
         )
 
-        self.doctor = DoctorProfile.objects.create(user=doctor_user)
+        cls.doctor = DoctorProfile.objects.create(user=doctor_user)
 
         res = httpx.get("https://api-adresse.data.gouv.fr/search/?q=16+rue+du+paraclet+77160")
         address = res.json()["features"][0]["properties"]["label"]
         patient_user = User.objects.create_user(
             username='patient', email='patient@email.com', password='patient_password'
         )
-        self.patient = PatientProfile.objects.create(
+        cls.patient = PatientProfile.objects.create(
             user=patient_user,
             address=address,
             birth_date=datetime.date(year=1995, month=2, day=18)
         )
 
-        self.doctor.patients.add(self.patient)
+        cls.doctor.patients.add(cls.patient)
+        cls.doctor.save()
 
     def test_doctor_authentication(self):
         res = self.client.get("http://127.0.0.1:8000/")
-        result = self.client.login(username=self.doctor.get_username(), password=self.doctor.user.password)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(self.doctor)
+
+        result = self.client.login(username=self.doctor.get_username(), password='doctor_password')
         self.assertTrue(result)
 
         self.client.logout()
 
     def test_user_authentication(self):
         result = self.client.login(
-            username=self.patient.get_username(), password=self.patient.user.password
+            username=self.patient.get_username(), password='patient_password'
         )
         self.assertTrue(result)
-
         self.client.logout()
